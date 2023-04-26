c1489a11441b57c785ddda09cc98d20ab43ed43c
AbdallahAbujazar <abdallah.abujazar@iths.se>
2023-04-26 13:57:22 +0300

    upload db file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgres://postgres:root@localhost:5432/myapp'
+  connectionString: 'postgres://abdallah:n0Ex7epvypYkkxWS003FROqo5fR50H7N@dpg-ch4g5t4s3fvjtici8atg-a/myapp_ovz0'
 });
 
 module.exports = pool;
